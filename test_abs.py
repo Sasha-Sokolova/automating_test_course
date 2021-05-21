c387fed5aa706fc59988644ed817b2343f80a1eb
Sasha-Sokolova <mashapiterskaya@yandex.ru>
2021-05-21 08:31:12 +0300

    first_tests_with_pytest

---
+++ test_abs.py
@@ -0,0 +1,10 @@
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+
+test_abs1()
+
+test_abs2()
